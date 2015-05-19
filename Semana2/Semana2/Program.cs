662d424519030f83776d6a3c74679576461e765d
Xio95 <vargas.xiomara@gmail.com>
2015-05-18 20:22:08 -0600

    Ejemplo Corto 2

--- Semana2/Semana2/Program.cs
+++ Semana2/Semana2/Program.cs
@@ -11,6 +11,7 @@ namespace Semana2
         static void Main(string[] args)
         {
             Console.WriteLine("\nHola Mundo! :)");
+            Console.WriteLine("Nice to meet you");
             Console.ReadKey();
         }
     }
