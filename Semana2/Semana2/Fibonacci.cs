fbce46f6e157c5ca9b1a36e559ac7ad1d86eac2c
Xio95 <vargas.xiomara@gmail.com>
2015-05-18 20:53:17 -0600

    Ejercicio Fibonacci

---
+++ Semana2/Semana2/Fibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana2
+{
+    class Fibonacci
+    {
+        public int Calcular(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            if (n==2){
+                return 1;
+            } 
+             
+                return Calcular(n-1)+ Calcular(n-2);
+            } 
+        }
+    }
+
+
+            
+           
+
+        
+    
+      
+
--- Semana2/Semana2/Program.cs
+++ Semana2/Semana2/Program.cs
@@ -10,8 +10,12 @@ namespace Semana2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nHola Mundo! :)");
-            Console.WriteLine("Nice to meet you");
+            //Console.WriteLine("\nHola Mundo! :)");
+            //Console.WriteLine("Nice to meet you");
+            //Console.ReadKey();
+
+            Fibonacci prueba1 = new Fibonacci();
+            Console.WriteLine(prueba1.Calcular(5));
             Console.ReadKey();
         }
     }
