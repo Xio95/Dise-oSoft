e177b3c6d1495b14c9592f45a354e48543fcbf44
Xio95 <vargas.xiomara95@hotmail.com>
2015-06-15 19:33:36 -0600

    Laboratorio#1

---
+++ AplicacionPrincipal/LogicaAplicacion/CarritodeCompras.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion
+{
+    public class CarritodeCompras
+    {
+        List<Producto> ListaProductos = new List<Producto>();
+
+
+        public void Agregar(Producto ProductosLista)
+        {
+            //List<Producto> ListaProductos = new List<Producto>();
+
+            ListaProductos.Add(ProductosLista);
+
+        }
+
+        public void ImprimirCarrito()
+        {
+            foreach (Producto i in ListaProductos)
+
+                Console.WriteLine("\nIdentidicador: " + i.Identificador + "\nNombre: "+i.Nombre  + "\nFecha de registo: "+ i.FechaDeEmision);
+                
+        }
+
+    }
+}
+
---
+++ AplicacionPrincipal/LogicaAplicacion/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion
+{
+    public class Producto
+    {
+          public int Identificador;
+          public string Nombre;
+          public string FechaDeEmision;
+
+     public Producto(int Identificador, string Nombre,string FechaDeEmision){
+
+            this.Identificador= Identificador;
+            this.Nombre= Nombre;
+            this.FechaDeEmision= FechaDeEmision;
+        }
+        
+        }
+    }
+
