f88b7cde776fd867f750eb009a0c3c3a8fc551b6
Xio95 <vargas.xiomara@gmail.com>
2015-07-13 20:43:55 -0600

    Sem10

---
+++ ClassLibrary1/ClassLibrary1/ValidadorDeFechas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReglasCondicionFinanciera
+{
+    public class ValidaronDeFechas
+    {
+        // DateTime FechaDeVigencia;
+        //DateTime FechaDeRegistro;
+        // bool esValido;
+
+        public bool LaFechaDeVigenciaEsValida(DateTime fechaDeVigencia, DateTime fechaDeRegistro)
+        {
+            bool esValido = false;
+
+            if (fechaDeVigencia == fechaDeRegistro)
+            {
+                esValido = true;
+            }
+
+            if (fechaDeVigencia <= fechaDeRegistro)
+            {
+                esValido = false;
+            }
+
+            if (fechaDeVigencia >= fechaDeRegistro)
+            {
+                esValido = true;
+            }
+
+            return esValido;
+        }
+
+    }
+}
---
+++ ClassLibrary1/ClassLibrary2/ValidorDeCaracteristicasDeRedencion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaracteristicasDeRedencion
+{
+    public class ValidorDeCaracteristicasDeRedencion
+    {
+        public bool CaracteristicasDeRendencionSonValidas(bool permiteRedencion, double comision, int periodoMinimoDeRetencionEnDias)
+        { 
+            bool sonValidas= false;
+            if (permiteRedencion==false && comision== 0 && periodoMinimoDeRetencionEnDias==0){
+
+                sonValidas = true;
+            }
+             if (permiteRedencion==true && comision== 0.19 && periodoMinimoDeRetencionEnDias==30){
+
+                sonValidas = true;
+        }
+            if (permiteRedencion==true && comision== 0.10 && periodoMinimoDeRetencionEnDias==0){
+
+                sonValidas = false;
+        }
+             if (permiteRedencion==true && comision== 0 && periodoMinimoDeRetencionEnDias==30){
+
+                sonValidas = false;
+        }
+           return sonValidas;
+
+    }
+
+}
