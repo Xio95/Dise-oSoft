932bdbe136a7d978a8e5ecf0ad86d5f4fcec4889
Xio95 <vargas.xiomara@gmail.com>
2015-07-20 20:39:34 -0600

    Sem11

---
+++ Logica/Logica.UnitTest/Sumar_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logica.UnitTest
+{
+    [TestClass]
+    public class Sumar_Tests
+    {
+        [TestMethod]
+        public void ProbarQueSumaEnteros()
+        {
+            float esperado = 3;
+
+            Calculadora prueba = new Calculadora();
+            float obtenido = prueba.Sumar(1, 2);
+
+            Assert.AreEqual(esperado, obtenido);
+
+        }
+
+        [TestMethod]
+        public void ProbarQueSumaFlotantes()
+        {
+            float esperado = 1.1F;
+            Calculadora prueba = new Calculadora();
+            float obtenido = prueba.Sumar(1, 0.1F);
+
+            Assert.AreEqual(esperado, obtenido);
+
+        }
+    }
+}
